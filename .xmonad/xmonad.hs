06e578633df895d9f67de837061f73ca9c2c8753
Luke Palmer <luqui@google.com>
2013-12-18 11:12:31 -0800

    Two chrome launchers for my two profiles

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,4 +5,5 @@ import XMonad.Util.EZConfig
 main = xmonad $ gnomeConfig { modMask = mod4Mask
                             }
                 `additionalKeys`
-                [ ((mod4Mask .|. shiftMask, xK_backslash), spawn "google-chrome-beta") ]
+                [ ((mod4Mask, xK_backslash), spawn "google-chrome-beta --profile-directory=\"Default\"")
+                , ((mod4Mask .|. shiftMask, xK_backslash), spawn "google-chrome-beta --profile-directory=\"Profile 1\"") ]
