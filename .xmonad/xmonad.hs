2a0d349b0af74b69b51fd2ec895e5505d0bbb1fb
Luke Palmer <luqui@google.com>
2014-01-16 11:24:56 -0800

    Experimental xmonad setup with TabsOutliner

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -8,25 +8,38 @@ import XMonad.Layout (Tall(..))
 import XMonad.Layout.Reflect (reflectHoriz, REFLECTY(..))
 import XMonad.Layout.MultiToggle (single, Toggle(..), mkToggle)
 import XMonad.Hooks.ManageDocks (avoidStruts)
+import XMonad.Layout.LayoutScreens (layoutSplitScreen)
+import XMonad.Layout.TwoPane (TwoPane(..))
 
 main = xmonad conf
     where
-    leftSpaces = take 4 (workspaces conf)
-    rightSpaces = drop 4 (workspaces conf)
-    layoutHook = onWorkspaces leftSpaces (reflectHoriz layout) 
-               $ layout
+    leftSpaces = map (("left"++).show) [1..4]
+    rightSpaces = map (("right"++).show) [1..5]
+    orgSpace = "org"
+    workspaces = leftSpaces ++ rightSpaces ++ [orgSpace]
+    layoutHook = onWorkspaces leftSpaces (reflectHoriz layout) $ layout
     layout = avoidStruts (Tall 1 (3/100) (1/2) ||| Full)
-    conf = Gnome.gnomeConfig { modMask = mod4Mask, layoutHook = layoutHook } `EZ.additionalKeys` keys'
-    keys' = [ ((mod4Mask, xK_backslash), spawn "google-chrome-stable --profile-directory=\"Default\"")
+    startupHook = do
+      rescreen
+      Physical.viewScreen (Physical.P 1)
+      layoutSplitScreen 2 (TwoPane 0.85 0.15)
+    conf = Gnome.gnomeConfig { 
+      modMask = mod4Mask, 
+      layoutHook = layoutHook,
+      startupHook = startupHook,
+      workspaces = workspaces
+      } `EZ.additionalKeys` keys'
+    keys' = [ ((mod4Mask .|. shiftMask .|. mod1Mask, xK_Return), spawn "gnome-terminal") -- "override terminal"
+            , ((mod4Mask .|. shiftMask, xK_Return), spawn "google-chrome-stable --app-id=pnhechapfaindjhompbnflcldabbghjo") -- chrome terminal app
+            , ((mod4Mask, xK_backslash), spawn "google-chrome-stable --profile-directory=\"Default\"")
             , ((mod4Mask .|. shiftMask, xK_backslash), spawn "google-chrome-stable --profile-directory=\"Profile 1\"")
-            , ((mod4Mask, xK_slash), sendMessage $ Toggle REFLECTY)
             , ((0, xK_F12), spawn "gnome-screensaver-command --lock")
             ]
           -- screen 0 keys 1-4
           ++ concat (zipWith (switchKeys 0) leftSpaces  [xK_1..xK_4])
           -- screen 1 keys 5-9
-          ++ concat (zipWith (switchKeys 1) rightSpaces [xK_5..xK_9])
-    switchKeys scr i k = [ ((modMask conf, k), goWS)
-                         , ((modMask conf .|. shiftMask, k), windows (W.shift i) >> goWS) ]
-      where
-      goWS = Physical.viewScreen (Physical.P scr) >> windows (W.greedyView i)
+          ++ concat (zipWith (switchKeys 2) rightSpaces [xK_5..xK_9])
+          ++ [ ((modMask conf .|. shiftMask, xK_0), windows (W.shift orgSpace) >> goWS 1 orgSpace) ]
+    switchKeys scr i k = [ ((modMask conf, k), goWS scr i)
+                         , ((modMask conf .|. shiftMask, k), windows (W.shift i) >> goWS scr i) ]
+    goWS scr i = Physical.viewScreen (Physical.P scr) >> windows (W.greedyView i)
